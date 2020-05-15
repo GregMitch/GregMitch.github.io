9a9b91d3f4243797af3154b848379b610bfcfde9
Gregory Mitchell <grmitch1@gmail.com>
2020-05-15 11:58:30 -0400

    Fixed typo with Last name text field and variable names in submits.php file

--- submits.php
+++ submits.php
@@ -1,8 +1,8 @@
 <?php
  $path = 'submits.txt';
- if (isset($_POST['field1']) && isset($_POST['field2'])) {
+ if (isset($_POST['firstname']) && isset($_POST['lastname'])) {
     $fh = fopen($path,"a+");
-    $string = $_POST['field1'].' - '.$_POST['field2'];
+    $string = $_POST['firstname'].' - '.$_POST['lastname'];
     fwrite($fh,$string); // Write information to the file
     fclose($fh); // Close the file
  }

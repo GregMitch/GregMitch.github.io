812ffca8ed808e800d57b6f524b00f275c892a2f
Gregory Mitchell <grmitch1@gmail.com>
2020-05-15 12:05:38 -0400

    Github pages does not support server side languages such as php. Woops! You learn something new everyday.

--- submits.php
+++
@@ -1,9 +0,0 @@
-<?php
- $path = 'submits.txt';
- if (isset($_POST['firstname']) && isset($_POST['lastname'])) {
-    $fh = fopen($path,"a+");
-    $string = $_POST['firstname'].' - '.$_POST['lastname'];
-    fwrite($fh,$string); // Write information to the file
-    fclose($fh); // Close the file
- }
-?>

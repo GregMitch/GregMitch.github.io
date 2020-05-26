cadd2edc505685eb0c65f9450ce7904ee4056df6
Gregory Mitchell <grmitch1@gmail.com>
2020-05-26 14:14:35 -0400

    Added organic property to items and provided option of chosing multiple restricitons at once.

--- lab2/scripts/groceries.js
+++ lab2/scripts/groceries.js
@@ -1,66 +1,77 @@
 // Array of products, each product is an object with different fieldset
 // A set of ingredients should be added to products
 
+// Some vegtables and animals raised with horomones hence why their organic property is false.
 var products = [
 	{
 		name: "brocoli",
 		vegetarian: true,
 		glutenFree: true,
-		price: 1.99
+		price: 1.99,
+		organic: true
 	},
 	{
 		name: "twinkies",
 		vegetarian: false,
 		glutenFree: false,
-		price: 5.65
+		price: 5.65,
+		organic: false
 	},
 	{
 		name: "carrots",
 		vegetarian: true,
 		glutenFree: true,
-		price: 2.47
+		price: 2.47,
+		organic: true
 	},
 	{
 		name: "bread",
 		vegetarian: true,
 		glutenFree: false,
-		price: 2.35
+		price: 2.35,
+		organic: false
 	},
 	{
 		name: "bagel",
 		vegetarian: true,
 		glutenFree: false,
-		price: 2.97
+		price: 2.97,
+		organic: false
 	},
 	{
 		name: "beef",
 		vegetarian: false,
 		glutenFree: true,
-		price: 12.05
+		price: 12.05,
+		organic: true
 	},
 	{
 		name: "chicken",
 		vegetarian: false,
 		glutenFree: true,
-		price: 11.00
+		price: 11.00,
+		organic: false
 	},
 	{
 		name: "lettuce",
 		vegetarian: true,
 		glutenFree: true,
-		price: 1.97
+		price: 1.97,
+		organic: false
 	},
 	{
 		name: "apples",
 		vegetarian: true,
 		glutenFree: true,
-		price: 4.97
+		price: 4.97,
+		organic: true
 	},
 	{
 		name: "salmon",
 		vegetarian: false,
 		glutenFree: true,
-		price: 10.00
+		price: 10.00,
+		organic: false
 	}
 ];
 
@@ -72,7 +83,22 @@ var products = [
 function restrictListProducts(prods, restriction) {
 	let product_names = [];
 	for (let i=0; i<prods.length; i+=1) {
-		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
+		if ((restriction == "Vegetarian & GlutenFree & Organic") && (prods[i].vegetarian == true) && (prods[i].glutenFree == true) && (prods[i].organic == true)){
+			product_names.push(prods[i].name);
+		}
+		else if ((restriction == "Vegetarian & GlutenFree") && (prods[i].vegetarian == true) && (prods[i].glutenFree == true)){
+			product_names.push(prods[i].name);
+		}
+		else if ((restriction == "Organic & GlutenFree") && (prods[i].organic == true) && (prods[i].glutenFree == true)){
+			product_names.push(prods[i].name);
+		}
+		else if ((restriction == "Organic & Vegetarian") && (prods[i].organic == true) && (prods[i].vegetarian == true)){
+			product_names.push(prods[i].name);
+		}
+		else if ((restriction == "Organic") && (prods[i].organic == true)){
+			product_names.push(prods[i].name);
+		}
+		else if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
 			product_names.push(prods[i].name);
 		}
 		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){

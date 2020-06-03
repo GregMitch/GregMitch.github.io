e3a315bd3c73f820144f6ccffb5064a7c710c561
Gregory Mitchell <grmitch1@gmail.com>
2020-06-03 13:52:50 -0400

    Implemented sidebar instead of tab navigation

--- lab3/scripts/main.js
+++ lab3/scripts/main.js
@@ -1,22 +1,22 @@
 // This function is called when any of the tab is clicked
 // It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
 
-function openInfo(evt, tabName) {
+function openInfo(evt, navName) {
 
 	// Get all elements with class="tabcontent" and hide them
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
+	navcontent = document.getElementsByClassName("navcontent");
+	for (i = 0; i < navcontent.length; i++) {
+		navcontent[i].style.display = "none";
 	}
 
 	// Get all elements with class="tablinks" and remove the class "active"
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	navlinks = document.getElementsByClassName("navlinks");
+	for (i = 0; i < navlinks.length; i++) {
+		navlinks[i].className = navlinks[i].className.replace(" active", "");
 	}
 
 	// Show the current tab, and add an "active" class to the button that opened the tab
-	document.getElementById(tabName).style.display = "block";
+	document.getElementById(navName).style.display = "block";
 	evt.currentTarget.className += " active";
 
 }
